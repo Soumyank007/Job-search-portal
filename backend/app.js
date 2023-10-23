6ffc0b3886197a799729f5613fb8e99616c2c8ec
Soumyank Padhy <soumyank.padhy@gmail.com>
2023-10-23 20:42:41 +0530

    Feat:Initial Backend commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('DB connected'))
+.catch((err) => console.log(err));
+
+// Configure middleware
+app.use(cors());
+
+// Use Morgan for request logging
+app.use(morgan('dev'));
+
+// Use Body-Parser for parsing request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
