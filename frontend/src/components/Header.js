4ab628adf1431323069ad2073154a8c0ecf97f66
Soumyank Padhy <soumyank.padhy@gmail.com>
2023-10-24 09:52:41 +0530

    Feat:Add Search component in Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import { Box, styled } from '@mui/material'
 import React from 'react'
 import headerImage from '../images/career-bg.png';
+import SearchInputEl from './SearchInputEl';
 
 const Header = () => {
 
@@ -19,7 +20,7 @@ const Header = () => {
     return (
         <>
             <StyleHeader >
-
+                <SearchInputEl />
             </StyleHeader>
         </>
     )
